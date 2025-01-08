65d966f25d298a0f8ff7b1b9fb568cbc4cc52b41
Angelo Andres <98630996+AndresAngelo@users.noreply.github.com>
2025-01-09 05:43:34 +0800

    Add files via upload

---
+++ jsonFileHandler.py
@@ -0,0 +1,14 @@
+"""
+Your module description - jsonFileHandler
+"""
+
+import json
+
+def readJsonFile(fileName):
+    data = ""
+    try:
+        with open(fileName) as json_file:
+            data = json.load(json_file)
+    except IOError:
+        print("Could not read file")
+    return data
